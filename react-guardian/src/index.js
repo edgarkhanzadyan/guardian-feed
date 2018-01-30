6d7b8caeb14a56c9ea16043589310d40e45f6340
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2018-01-30 14:56:47 +0400

    configure redux-persist

--- react-guardian/src/index.js
+++ react-guardian/src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/components/App';
+
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import allReducers from './js/reducers';
 
-const store = createStore(allReducers);
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+import configureStore from './js/store/configureStore';
+let { store, persistor } = configureStore();
 
 store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
---
+++ react-guardian/src/js/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import allReducers from '..//reducers'
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, allReducers)
+
+export default () => {
+  let store = createStore(persistedReducer)
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
